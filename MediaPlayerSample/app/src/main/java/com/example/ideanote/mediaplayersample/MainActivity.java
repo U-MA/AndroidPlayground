f7333ab9c1685545a1c96df864160184dfeefead
U-MA <g.0224incident@gmail.com>
2015-11-01 23:58:13 +0900

    Change Progress setting percent

--- MediaPlayerSample/app/src/main/java/com/example/ideanote/mediaplayersample/MainActivity.java
+++ MediaPlayerSample/app/src/main/java/com/example/ideanote/mediaplayersample/MainActivity.java
@@ -146,7 +146,7 @@ public class MainActivity extends AppCompatActivity {
         mediaPlayer.setOnBufferingUpdateListener(new MediaPlayer.OnBufferingUpdateListener() {
             @Override
             public void onBufferingUpdate(MediaPlayer mp, int percent) {
-                progressBar.setProgress(percent);
+                progressBar.setSecondaryProgress(percent);
             }
         });
     }
