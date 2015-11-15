918b7258135b5594d245cd96d4057cdf4525413c
U-MA <g.0224incident@gmail.com>
2015-11-15 23:34:07 +0900

    Pass First test

---
+++ shakyo/EspressoBasicSample/app/src/androidTest/java/com/example/ideanote/espressobasicsample/ChangeTextBehaviorTest.java
@@ -0,0 +1,42 @@
+package com.example.ideanote.espressobasicsample;
+
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeTextIntoFocusedView;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class ChangeTextBehaviorTest {
+
+    public static final String STRING_TO_BE_TYPED = "Basic Sample";
+
+    // これなんだろう
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Test
+    public void changeText_sameActivity() {
+        // Type text and then press the button;
+        onView(withId(R.id.editTextUserInput))
+                .perform(typeText(STRING_TO_BE_TYPED), closeSoftKeyboard());
+        onView(withId(R.id.changeTextBt)).perform(click());
+
+        // Check that the text was changed
+        onView(withId(R.id.textToBeChanged)).check(matches(withText(STRING_TO_BE_TYPED)));
+    }
+}
---
+++ shakyo/EspressoBasicSample/app/src/main/java/com/example/ideanote/espressobasicsample/ShowTextActivity.java
@@ -0,0 +1,39 @@
+package com.example.ideanote.espressobasicsample;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ShowTextActivity extends AppCompatActivity {
+
+    private final static String EXTRA_MESSAGE_TEXT = "extra_message_text";
+
+    public static Intent createIntent(Context context, String text) {
+        Intent intent = new Intent(context, ShowTextActivity.class);
+        intent.putExtra(EXTRA_MESSAGE_TEXT, text);
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_text);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
