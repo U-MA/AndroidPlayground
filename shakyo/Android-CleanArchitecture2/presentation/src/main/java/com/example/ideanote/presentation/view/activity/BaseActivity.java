398bdc029725fe01c7b77c8133468a7b707f3856
U-MA <g.0224incident@gmail.com>
2015-12-29 23:38:46 +0900

    Initial commit

---
+++ shakyo/Android-CleanArchitecture2/presentation/src/main/java/com/example/ideanote/presentation/view/activity/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.example.ideanote.presentation.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Inject
+    Navigator navigator;
+}
